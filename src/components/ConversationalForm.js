ca6156f042e0380e92403a53125ff248d60ca4ed
Nishoake <nsribava@uwaterloo.ca>
2021-01-22 19:40:44 -0500

    Added ref object, formFields and submitCallback function

--- src/components/ConversationalForm.js
+++ src/components/ConversationalForm.js
@@ -3,6 +3,35 @@ import ConversationalForm from 'conversational-form'
 
 export default function ConversationalForm(){
   
+  // Creating a ref
+  const ref = useRef()
+
+  // Defining the fields / questions for the conversational-form
+  const formFields = [
+    {
+      'tag': 'input',
+      'type': 'text',
+      'name': 'firstname',
+      'cf-questions': 'What is your firstname?'
+    },
+    {
+      'tag': 'input',
+      'type': 'text',
+      'name': 'lastname',
+      'cf-questions': 'What is your lastname?'
+    }
+  ]
+
+  // Defining the submitCallback function once form is submitted
+  function submitCallback() {
+    // get the FormData object in JSON format
+    let formDataSerialized = cf.getFormData(true)
+    console.log(`Formdata, obj: ${formDataSerialized}`)
+
+    // Provide user with response after form submission
+    cf.addRobotChatResponse("You are done. Check the dev console for form data output.")
+  }
+
   return(
     <div></div>
   )
